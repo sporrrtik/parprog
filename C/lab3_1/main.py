b7e9c377dfe13590ba1fc59d3a05725bd57b7cf9
sporrrtik <81933050+sporrrtik@users.noreply.github.com>
2023-10-14 11:55:46 +0300

    Add files via upload

--- C/lab3_1/main.py
+++ C/lab3_1/main.py
@@ -1,14 +1,25 @@
 import os
 n1 = 1
-n2 = 397
+n2 = 9900
+threads = 10
+schedule = "static"
+time_delta = []
 delta = (n2 - n1) // 10
-filenames = ("lab1-par-1", "lab1-par-2", "lab1-par-4", "lab1-par-10")
-comp= "gcc"
-os.system(f"make clean")
-os.system(f"make gcc-seq")
-for filename in filenames:
-    now_len = n1
-    print(f"NOW {filename}, N = {now_len}=================================\n===========================================================\n===========================================================\n===========================================================\n===========================================================\n===========================================================")
-    for i in range(10):
-        now_len += delta
-        os.system(f"./{filename} {now_len} 10 {comp}-{filename}")
+
+filename = "omp"
+
+os.system(f"make omp_clean")
+os.system(f"make omp")
+for i in range(10):
+    n1 += delta
+    os.system(f"./lab3_omp {n1} 10 omp_{threads}_{schedule}")
+        
+    with open(f"./omp_{threads}_{schedule}_delt.txt", "r") as f:
+        times = [int(line.replace("\n", "").split()[1]) for line in f.readlines()]
+        time_delta += [[n1, min(times)]]
+
+    with open(f"./omp_{threads}_{schedule}_delt.txt", "w") as f:
+        pass
+      
+with open(f"./omp_{threads}_{schedule}_delt.txt", "w") as f:
+    f.write(str(time_delta))
